2f8cfedba9984b360f80bbd46d42183166721b32
davidroman-hub <jobroman83@gmail.com>
2020-05-27 18:27:42 +0100

    adding header

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React,{Fragment} from 'react';
+
+import Logo from '../images/Logo.png'
 
 
 
 
 const Home = () => {
   return (
-    <h1> hola soy home </h1>
+    <Fragment>
+    <header>
+      <div className='header-logo'>
+        <img className='img img-fluid'
+         style={{maxHeight:''}} 
+         src={Logo} alt="Logo-principal"/>
+      </div>
+    </header>
+    <div>
+      <p className="titulo-naranja-neon texto-regular">
+        Lost fusion
+      </p>
+    </div>
+  </Fragment>
   )
+  
 }
 
 export default Home;
