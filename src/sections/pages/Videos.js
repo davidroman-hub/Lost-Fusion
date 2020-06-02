14e9625d5a43bcfb2071efec796507a3c98d587d
davidroman-hub <jobroman83@gmail.com>
2020-06-02 01:57:06 +0100

    changing font

--- src/sections/pages/Videos.js
+++ src/sections/pages/Videos.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
+
 
 const Videos = () => {
     
     return (
-        <h1>Soy Videos</h1>
+      <ReactPlayer url='https://www.youtube.com/watch?v=TSloH95Kk6g'/>
     )
 }
 
