8b4e7350a2d79228c4f400dab9127cb94afa7cc5
davidroman-hub <jobroman83@gmail.com>
2020-06-02 05:51:49 +0100

    fixing logo

--- src/sections/pages/Videos.js
+++ src/sections/pages/Videos.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import ReactPlayer from 'react-player'
-
+import './Videos.scss'
 
 const Videos = () => {
     
     return (
-      <ReactPlayer url='https://www.youtube.com/watch?v=TSloH95Kk6g'/>
+<Fragment>
+  <br/>
+  <br/>
+<div className='titles-descPr text-center'> VIDEOS </div>
+  <br/>
+  <br/>
+      <div className='animation text-center'>Crónicas Perdidas cap. 1</div>
+  <br/>     
+      <div className="videos">
+          <ReactPlayer url='https://youtu.be/vsa_yncP-JI'/>
+      </div>
+  <br/>
+  <br/>   
+  <div className='animation text-center'>Lost Fusión - Serpiente (oficial) </div>
+  <br/>    
+      <div className="videos">
+          <ReactPlayer url='https://youtu.be/RFssByTlhHM'/>
+      </div>
+  <br/>
+  <br/>
+  <div className='animation text-center'>Lost Fusión - Costa Linda (Lyric)</div>
+  <br/>        
+      <div className="videos">
+          <ReactPlayer url='https://youtu.be/XOr6c7n4SUc'/>
+      </div>
+  <br/>
+  <br/> 
+  <div className='animation text-center'>Lost Fusión - Guajira (En Vivo Radio UNAM)</div>
+  <br/>   
+      <div className="videos">
+          <ReactPlayer url='https://youtu.be/Ktt1sSlLq3Q'/>
+      </div>   
+  <br/>
+  <br/> 
+
+     
+</Fragment>      
     )
 }
 
